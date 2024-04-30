8d9200aac3074b423670a0e093169df0fcd34e25
shinzo <46932317+Pahasara@users.noreply.github.com>
2024-04-30 23:03:33 +0530

    Update Program.cs

--- Zero.CLI/Program.cs
+++ Zero.CLI/Program.cs
@@ -1,16 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Data.SQLite;
-using Zero.Core;
+using Zero.CLI;
 
 public class Program
 {
     private static void Main(string[] args)
     {
-        Database database = new Database();
         Dashboard dashboard = new Dashboard();
-        Navigation navigation = new Navigation();
-
         SQLiteConnection conn = new SQLiteConnection("Data Source = database.db; version = 3; New = True; Compress = True;");
+        
         try
         {
             conn.Open();
