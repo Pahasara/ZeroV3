a7e4226f9a0a1a0f57785159a0c0305b76eed111
shinzo <46932317+Pahasara@users.noreply.github.com>
2024-05-15 00:20:15 +0530

    Create Arithmetic.cs

---
+++ Zero.Core/Arithmetic.cs
@@ -0,0 +1,29 @@
+﻿namespace Zero.Core
+{
+    public class Arithmetic
+    {
+        public float GetProgress(string value, string tot)
+        {
+            Int32.TryParse(value, out int progress);
+            Int32.TryParse(tot, out int total);
+
+            float percentage = (float)progress / total;
+
+            return percentage;
+        }
+
+        public static int ParseInt(string str)
+        {
+            int num = 0;
+            try
+            {
+                num = Int32.Parse(str);
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+            return num;
+        }
+    }
+}
