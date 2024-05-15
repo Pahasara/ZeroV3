bbd4bc590ebcfd907b01e2983298019c0d5c0017
shinzo <46932317+Pahasara@users.noreply.github.com>
2024-05-15 20:20:43 +0530

    Create Help.cs
    Defines help methods

---
+++ Zero.CLI/Help.cs
@@ -0,0 +1,20 @@
+﻿namespace Zero.Core
+{
+    public class Help
+    {
+        public static void Dashboard()
+        {
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("                  Help: Dashboard");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("n        Go to Next TVShow");
+            Console.WriteLine("b        Go to Previous TVShow");
+            Console.WriteLine("++       Forward progress");
+            Console.WriteLine("--       Backward progress");
+            Console.WriteLine("+++      Finish progress");
+            Console.WriteLine("r        Reset progress");
+            Console.WriteLine("del      Delete current TVShow");
+            Console.WriteLine("----------------------------------------------------");
+        }
+    }
+}
