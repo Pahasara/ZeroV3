2743ccfd4fbb46bd82673e3ac99df187c3166cd4
シンゾー <46932317+Pahasara@users.noreply.github.com>
2024-07-10 00:12:58 +0530

    Fixed some issues

--- Zero.CLI/Program.cs
+++ Zero.CLI/Program.cs
@@ -1,27 +1,29 @@
 ﻿using System.Data.SQLite;
 using Zero.CLI;
+using Zero.Core;
 
 public class Program
 {
     private static void Main(string[] args)
     {
         const string URL = "github.com/pahasara/ZeroV3";
-        Dashboard dashboard = new Dashboard();
-        SQLiteConnection conn = new SQLiteConnection("Data Source = database.db; version = 3; New = True; Compress = True;");
-        
+
         try
         {
+            SQLiteConnection conn = new SQLiteConnection("Data Source = database.db; version = 3; New = True; Compress = True;");
             conn.Open();
-            while (true)
-            {
-                dashboard.ShowMenu();
-            }
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error connecting to database: {ex.Message}");
-            Console.WriteLine($"Report this issue on {URL}.");
+            Console.WriteLine($"If this keep happening, report this issue on {URL}.");
             Console.ReadKey();
+        }
+        finally
+        {
+            Dashboard dashboard = new Dashboard();
+            dashboard.ShowMenu();
+            dashboard.ShowDashboard();
         }
     }
 }
