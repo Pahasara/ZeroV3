3b027c334d42880a588877f64d9ec6f7b41a4c71
シンゾー <46932317+Pahasara@users.noreply.github.com>
2024-07-15 23:27:30 +0530

    Update Dashboard.cs

--- Zero.CLI/Dashboard.cs
+++ Zero.CLI/Dashboard.cs
@@ -5,51 +5,54 @@ namespace Zero.CLI
     public class Dashboard
     {
         Navigation navi = new Navigation();
-        private string choice = "";
+        private string choice = "b";
 
         public void ShowMenu()
         {
             Console.Clear();
-            Console.WriteLine("----------------------------------------------------");
-            Console.WriteLine($"                Zero TVSM v{Info.Version}");
-            Console.WriteLine("----------------------------------------------------");
-            Console.WriteLine("Type help for for more information.");
-            Console.WriteLine("Press enter to exit from Zero TVSM.\n");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine($"            Zero TVSM v{Info.Version}");
+            Console.WriteLine("--------------------------------------------");
         }
 
         public void ShowDashboard()
         {
-            do
+            while (choice != "exit")
             {
                 Console.WriteLine();
                 Console.Write("Command: ");
                 choice = Console.ReadLine();
-
-                if (choice == "add")
-                {
-                    navi.AddShow();
-                }
-                else if (choice == "reset")
-                {
-                    navi.ResetProgress();
-                }
-                else if (choice == "del")
-                {
-                    navi.DeleteShow();
-                }
-                else if (choice == "help")
-                {
-                    Help.Dashboard();
-                }
-                else
-                {
-                    TryNavigate();
-                }
+                CheckChoice(choice);
             }
-            while (choice != "");
         }
 
-        private void TryNavigate()
+        public void CheckChoice(string choice)
+        {
+            if (string.IsNullOrWhiteSpace(choice))
+            {
+                choice = "exit";
+            }
+            switch (choice.ToLower())
+            {
+                case "add":
+                    navi.AddShow();
+                    break;
+                case "reset":
+                    navi.ResetProgress();
+                    break;
+                case "del":
+                    navi.DeleteShow();
+                    break;
+                case "help":
+                    Help.Dashboard();
+                    break;
+                default:
+                    TryNavigate();
+                    break;
+            }
+        }
+
+        public void TryNavigate()
         {
             int maxRow = navi.GetMaxRow();
             if (maxRow > 0)
@@ -58,80 +61,66 @@ namespace Zero.CLI
             }
             else
             {
-                Console.WriteLine("No TVShow has been saved. Pleas use 'add' command to add new entries.");
+                Console.WriteLine("No TVShow has been saved. Please use 'add' command to add new entries.");
             }
         }
 
         private void Navigate()
         {
-            if (choice == "n")
+            switch (choice.ToLower())
             {
-                navi.Navigate("next");
-            }
-            else if (choice == "b")
-            {
-                navi.Navigate("back");
-            }
-            else if (choice == "del")
-            {
-                navi.DeleteShow();
-            }
-            else
-            {
-                ChangeProgress();
+                case "n":
+                    navi.Navigate("next");
+                    break;
+                case "b":
+                    navi.Navigate("back");
+                    break;
+                case "clear":
+                    Console.Clear();
+                    break;
+                case "++":
+                    navi.Forward();
+                    navi.Navigate();
+                    break;
+                case "--":
+                    navi.Backward();
+                    navi.Navigate();
+                    break;
+                case "r":
+                    navi.ResetProgress();
+                    navi.Navigate();
+                    break;
+                case "+++":
+                    navi.FinishProgress();
+                    navi.Navigate();
+                    break;
+                default:
+                    navi.Search(choice);
+                    break;
             }
             ShowMenu();
             ShowData();
             ShowRowNumber();
         }
 
-        private void ChangeProgress()
-        {
-            if (choice == "++")
-            {
-                navi.Forward();
-                navi.Navigate();
-            }
-            else if (choice == "--")
-            {
-                navi.Backward();
-                navi.Navigate();
-            }
-            else if (choice == "r")
-            {
-                navi.ResetProgress();
-                navi.Navigate();
-            }
-            else if (choice == "+++")
-            {
-                navi.FinishProgress();
-                navi.Navigate();
-            }
-            else
-            {
-                navi.Search(choice);
-            }
-        }
-
         private void ShowData()
         {
-            Console.WriteLine();
-            Console.WriteLine("----------------------------------------------------");
             Console.WriteLine($"Index   : {navi.GetIndex()}");
             Console.WriteLine($"Show    : {navi.GetShow()}");
-            Console.WriteLine($"Rating  : {navi.GetRating()}");
             Console.WriteLine($"Current : {navi.GetCurrent()}");
             Console.WriteLine($"Total   : {navi.GetTotal()}");
-            Console.WriteLine($"Progress: {navi.GetCurrent()}/{navi.GetTotal()}");
-            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine($"Progress: {navi.GetCurrent()}/ {navi.GetTotal()} ({navi.GetProgress()}%)");
         }
 
         private void ShowRowNumber()
         {
-            int currentRow = navi.currentRow + 1;
+            int currentRow = navi.currentRow + 1; // To show virtual indexes: from 1 to ..
+            if (currentRow == 0)
+                currentRow = 1;
             int maxRows = navi.maxRow;
-            Console.WriteLine($"                       {currentRow}/ {maxRows}");
-            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine($"                   {currentRow}/ {maxRows}");
+            Console.WriteLine("--------------------------------------------");
         }
     }
 }
